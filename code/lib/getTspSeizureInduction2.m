3b4a9980436111ed55383a48a2276f31255b8dd1
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-17 21:05:05 +0200

    Create getTspSeizureInduction2.m

---
+++ code/lib/getTspSeizureInduction2.m
@@ -0,0 +1,17 @@
+function [RecInfo,DataStruct] = getTspSeizureInduction2(metainfo, ID, closed)
+% get timestamps of seizure induction, detections of rat 1, rat 2
+% Copyright (c) 2020 Yuichi Takeuchi
+
+RecInfo = cell(1, size(metainfo,1));
+DataStruct = cell(1, size(metainfo,1));
+for i = 1:size(metainfo,1)
+    [tmpRecInfo] = getRecInfo(metainfo{i,1}, metainfo{i,2}, metainfo{i,3}, metainfo{i,4}, metainfo{i,5}, 500);
+    [tmpDataStruct] = getDataStruct(tmpRecInfo);
+    RecInfo{i} = tmpRecInfo;
+    DataStruct{i}(1) = tmpDataStruct;
+    DataStruct{i}(2) = tmpDataStruct; % duplication
+end
+save(['tmp/' ID '_' num2str(closed) '_RecInfo.mat' ], 'RecInfo')
+save(['tmp/' ID '_' num2str(closed) '_DataStruct.mat'], 'DataStruct')
+
+end
